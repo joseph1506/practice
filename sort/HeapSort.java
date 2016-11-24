0079b7b43c67ea8ea2791d55d9fb62122c9a9396
joseph1506 <josephin.angel@gmail.com>
2016-11-24 09:59:26 +0530

    Joseph|Other algorithms

---
+++ sort/HeapSort.java
@@ -0,0 +1,14 @@
+package practice.sort;
+
+public class HeapSort {
+
+    public static void main(String[] args) {
+        int[] numbers={1,7,2,90,23,22,21,56,89};
+        HeapSort.sort(numbers);
+        System.out.println("Sorted Array--->"+numbers);
+    }
+
+    private static void sort(int[] numbers) {
+
+    }
+}
